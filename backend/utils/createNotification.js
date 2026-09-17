25475f35eb66e34b9e24d81ea858882f37193e2b
Vincent <jeniferamose@gmail.com>
2026-09-17 11:36:35 +0530

    feat: initialize frontend and backend project structure with dependencies

--- backend/utils/createNotification.js
+++
@@ -1,7 +0,0 @@
-const Notification = require('../models/Notification');
-
-const createNotification = async ({ companyId, userId, message, type }) => {
-  return Notification.create({ companyId, userId, message, type });
-};
-
-module.exports = createNotification;
